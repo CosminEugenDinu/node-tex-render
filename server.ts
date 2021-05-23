cc5f01203dd3183b8e1daeccd5149c987f5d39dc
Cos <cosmineugendinu@gmail.com>
2021-05-23 09:22:38 +0300

    refactor: Source html files

--- server.ts
+++ server.ts
@@ -1,42 +1,26 @@
 import fs from "fs";
 import express from "express";
-import { texToSvg } from "./src/tex-to-svg"
-import { parse } from 'node-html-parser';
-
-const myTeXEquation = "\\frac{n!}{k!(n-k)!} = \\binom{n}{k}";
-
-// const SVGEquation = texToSvg(myTeXEquation);
+import { texToSvg } from "./src/tex-to-svg";
+import { parse } from "node-html-parser";
 
 const app = express();
 
-const root = parse(`
-<i style="font-size:xxx-large">\\frac{n!}{k!(n-k)!} = \\binom{n}{k};</i>
-<i style="font-size:small">\\frac{n!}{k!(n-k)!} = \\binom{n}{k};</i>
-`);
 app.get("/", (req, res) => {
-  const allP = root.querySelectorAll("i");
-  for (const el of allP) {
-    el.innerHTML = texToSvg(el.innerHTML);
-  }
-  res.send(allP.join(''));
-});
-
-app.get("/html", (req, res) => {
-  fs.readFile('./documents/hello.html', 'utf8' , (err, data) => {
+  fs.readFile("./documents/src/hello.html", "utf8", (err, data) => {
     if (err) {
-      console.error(err)
-      return
+      console.error(err);
+      return;
     }
     const root = parse(data);
     const all_i = root.querySelectorAll("i");
     for (const i of all_i) {
-      i.innerHTML = texToSvg(i.innerHTML);
+      const texStr = i.innerHTML;
+      const SVGEquation = texToSvg(texStr);
+      i.innerHTML = SVGEquation;
     }
     res.send(root.toString());
-    // console.log(root.toString())
-  })
-})
-
+  });
+});
 
 app.listen(8080, () => {
   console.log("server started on port 8080");
