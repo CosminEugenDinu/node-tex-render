6f9f3900ed3acbacbb48ddf8d0b116f1287e8cee
Cos <cosmineugendinu@gmail.com>
2021-05-23 12:24:25 +0300

    feat: Code syntax highlight serverside only

--- server.ts
+++ server.ts
@@ -1,9 +1,12 @@
 import fs from "fs";
+import path from "path";
 import express from "express";
 import { texToSvg } from "./src/tex-to-svg";
 import { parse } from "node-html-parser";
 
 const app = express();
+
+app.use("/", express.static(path.join(__dirname, "./public")));
 
 app.get("/", (req, res) => {
   fs.readFile("./documents/src/hello.html", "utf8", (err, data) => {
@@ -20,6 +23,23 @@ app.get("/", (req, res) => {
     }
     res.send(root.toString());
   });
+});
+
+const head = `<head>
+<link rel="stylesheet" href="/github.css">
+</head>`;
+const str = `
+\`\`\`py
+print("hello")
+\`\`\`
+`;
+
+import { process } from "./src/highlight";
+app.get("/s", (req, res) => {
+  const out = process(str);
+  out.then(r=>res.send(head + '<body>'+r.contents+'</body>'));
+  // out.then(r=>res.send(r.contents));
+  // res.send()
 });
 
 app.listen(8080, () => {
---
+++ src/highlight.ts
@@ -0,0 +1,28 @@
+const unified = require('unified')
+const markdown = require('remark-parse')
+const remark2rehype = require('remark-rehype')
+const highlight = require('rehype-highlight')
+const html = require('rehype-stringify')
+
+// import unified from "unified";
+// import markdown from "remark-parse";
+// import remark2rehype from "remark-rehype";
+// import highlight from "rehype-highlight";
+// import html from "rehype-stringify";
+
+
+
+const processor = unified()
+    // Transform markdown into a markdown syntax tree
+    .use(markdown)
+    // Transform markdown syntax tree to html syntax tree
+    .use(remark2rehype)
+    // Traverse html syntax tree to apply code highlighting to content within code tags
+    .use(highlight)
+    // Transform html syntax tree to string to send to the client
+    .use(html)
+
+
+export async function process(str: string) {
+    return await processor.process(str);
+}
