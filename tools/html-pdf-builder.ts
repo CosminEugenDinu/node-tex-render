f8727061646ab80c958a9e2d7eccbc101bb348a7
Cos <cosmineugendinu@gmail.com>
2021-06-08 23:38:55 +0300

    chore: Tools for build pdf

--- tools/html-pdf-builder.ts
+++ tools/html-pdf-builder.ts
@@ -1,13 +1,15 @@
 const puppeteer = require("puppeteer");
 
+const articleDirName = "pedagogie1";
+
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto("http://localhost:8080/articles/subj", {
+  await page.goto(`http://localhost:8080/articles/${articleDirName}`, {
     waitUntil: "networkidle0",
   });
   await page.pdf({
-    path: "./articles/built/FAI-Subiecte.pdf",
+    path: `./articles/built/${articleDirName}.pdf`,
     format: "A6",
     printBackground: true,
   });
