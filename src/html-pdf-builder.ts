fb64dc9d757f34e43e6f287b5da45b311502378e
Cos <cosmineugendinu@gmail.com>
2021-06-08 23:05:24 +0300

    feat: Save html as pdf

---
+++ src/html-pdf-builder.ts
@@ -0,0 +1,16 @@
+const puppeteer = require("puppeteer");
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("http://localhost:8080/articles/subj", {
+    waitUntil: "networkidle0",
+  });
+  await page.pdf({
+    path: "./articles/build/FAI-Subiecte.pdf",
+    format: "A6",
+    printBackground: true,
+  });
+
+  await browser.close();
+})();
