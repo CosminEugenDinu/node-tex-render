eb2830cb12fab678c4bef53fb620cc9c588918b7
Cos <cosmineugendinu@gmail.com>
2021-06-08 23:32:05 +0300

    refactor: Every article in it's own folder

--- server.ts
+++ server.ts
@@ -7,9 +7,11 @@ const app = express();
 app.use("/", express.static(path.join(__dirname, "./public")));
 
 app.get("/articles/:name", async (req, res) => {
-  const articleRelPath = `./articles/src/${req.params.name}.html`;
-  const embedStyles = ["./public/style.css", "./public/syntax-light.css"];
-  const parsedHtml = await htmlTexBuilder(articleRelPath, embedStyles);
+  const articleRelPath = `./articles/src/${req.params.name}`;
+  const articleHtml = path.join(articleRelPath, "index.html");
+  const articleStyle = path.join(articleRelPath, "style.css");
+  const embedStyles = [articleStyle];
+  const parsedHtml = await htmlTexBuilder(articleHtml, embedStyles);
   res.send(parsedHtml);
 });
 
